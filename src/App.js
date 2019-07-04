eedcc41bf0c5938ef47fe968acb92b5a26df54df
sriharsha551 <sanampudisriharsha@gmail.com>
2019-07-04 21:28:56 +0530

    user page select

--- src/App.js
+++ src/App.js
@@ -231,7 +231,6 @@ class App extends React.Component {
   };
 
   render() {
-    console.log(sessionStorage.getItem("userRole"))
     return (
       <div>
         <div style={{ backgroundColor: "#1b181a" }}>
--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -2,7 +2,7 @@ import React from "react";
 import tnpbase from "../api/tnpbase";
 
 class AddUser extends React.Component {
-  state = { username: "", password: "", role: "", text: "", branch: "" };
+  state = { username: "", password: "", role: "", text: "", branch: "" ,display:""};
 
   addUser = () => {
     this.setState({ text: "loading" });
@@ -24,58 +24,101 @@ class AddUser extends React.Component {
   };
 
   render() {
-    return (
-      <div className="ui container">
-        <div className="ui form">
-          <div className="field">
-            <label>Username</label>
-            <input
-              type="text"
-              value={this.state.username}
-              onChange={e => this.setState({ username: e.target.value })}
-            />
+    if(this.state.role === "" || this.state.role === "TPO"){
+      return (
+        <div className="ui container">
+          <div className="ui form">
+            <div className="field">
+              <label>Username</label>
+              <input
+                type="text"
+                value={this.state.username}
+                onChange={e => this.setState({ username: e.target.value })}
+              />
+            </div>
+            <div className="field">
+              <label>Password</label>
+              <input
+                type="password"
+                value={this.state.password}
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+            </div>
+            <div className="field">
+              <label>Role</label>
+              <select
+                value={this.state.role}
+                onChange={e => {
+                  this.setState({ role: e.target.value })}}
+              >
+                <option value="">Select Role</option>
+                <option value="TPO">TPO</option>
+                <option value="PCO">PCO</option>
+              </select>
+            </div>
+            <button className="ui secondary button" onClick={this.addUser}>
+              Add User
+            </button>
+            <div>{this.state.text}</div>
           </div>
-          <div className="field">
-            <label>Password</label>
-            <input
-              type="password"
-              value={this.state.password}
-              onChange={e => this.setState({ password: e.target.value })}
-            />
-          </div>
-          <div className="field">
-            <label>Role</label>
-            <select
-              value={this.state.role}
-              onChange={e => this.setState({ role: e.target.value })}
-            >
-              <option value="">Select Role</option>
-              <option value="TPO">TPO</option>
-              <option value="pco">PCO</option>
-            </select>
-          </div>
-          <div className="field">
-            <label>Branch</label>
-            <select
-              value={this.state.branch}
-              onChange={e => this.setState({ branch: e.target.value })}
-            >
-              <option value="">Select Branch</option>
-              <option value="05">CSE</option>
-              <option value="12">IT</option>
-              <option value="04">ECE </option>
-              <option value="02">EEE </option>
-              <option value="01">CIVIL </option>
-              <option value="03">MECH</option>
-            </select>
-          </div>
-          <button className="ui secondary button" onClick={this.addUser}>
-            Add User
-          </button>
-          <div>{this.state.text}</div>
         </div>
-      </div>
-    );
+      );
+    }
+    else if(this.state.role === "PCO")
+    {
+      return (
+        <div className="ui container">
+          <div className="ui form">
+            <div className="field">
+              <label>Username</label>
+              <input
+                type="text"
+                value={this.state.username}
+                onChange={e => this.setState({ username: e.target.value })}
+              />
+            </div>
+            <div className="field">
+              <label>Password</label>
+              <input
+                type="password"
+                value={this.state.password}
+                onChange={e => this.setState({ password: e.target.value })}
+              />
+            </div>
+            <div className="field">
+              <label>Role</label>
+              <select
+                value={this.state.role}
+                onChange={e => this.setState({ role: e.target.value })}
+              >
+                <option value="">Select Role</option>
+                <option value="TPO">TPO</option>
+                <option value="pco">PCO</option>
+              </select>
+            </div>
+            <div className="field" style={{display:this.state.display}}>
+              <label>Branch</label>
+              <select
+                value={this.state.branch}
+                onChange={e => this.setState({ branch: e.target.value })}
+              >
+                <option value="">Select Branch</option>
+                <option value="05">CSE</option>
+                <option value="12">IT</option>
+                <option value="04">ECE </option>
+                <option value="02">EEE </option>
+                <option value="01">CIVIL </option>
+                <option value="03">MECH</option>
+              </select>
+            </div>
+            <button className="ui secondary button" onClick={this.addUser}>
+              Add User
+            </button>
+            <div>{this.state.text}</div>
+          </div>
+        </div>
+      );
+    }
   }
 }
 
